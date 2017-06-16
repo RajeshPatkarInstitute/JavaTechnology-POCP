9e575da8d12712ecfdf22de45afd48861765648f
Rajesh Patkar <patkar@rajeshpatkar.com>
2017-06-16 19:54:47 +0530

    Create Client.java

---
+++ Java-Language/Object-Oriented-Programming/Inheritance/Stack-Case-Study/2. AStack with Auditing/Client.java
@@ -0,0 +1,24 @@
+package com.rajeshpatkar;
+
+public class Client1 {
+    public static void useAStack(AStack s){
+        
+        s.push(10);
+        System.out.println("Client1 pushed 10");
+        
+        s.push(20);
+        System.out.println("Client1 pushed 20");
+        
+        s.push(30);
+        System.out.println("Client1 pushed 30");
+        
+        System.out.println("Client1 popped "+s.pop());
+    }
+    
+    public static void AuditorAStack(AStack s){
+        System.out.println("Entering Auditor for AStack");
+        System.out.println("Push Count is " + s.getPushcount());
+        System.out.println("Pop Count is "+s.getPopcount());
+        System.out.println("Leaving Auditor for AStack");
+    }
+}
