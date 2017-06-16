e9f03738d67e7a60d945ac5316a0c46a0d9ea01c
Rajesh Patkar <patkar@rajeshpatkar.com>
2017-06-16 20:39:25 +0530

    Update Admin.java

--- Java-Language/Object-Oriented-Programming/Inheritance/Stack-Case-Study/4. LStack with Auditing/Admin.java
+++ Java-Language/Object-Oriented-Programming/Inheritance/Stack-Case-Study/4. LStack with Auditing/Admin.java
@@ -2,10 +2,6 @@ package com.rajeshpatkar;
 
 public class StackCaseStudy {
     public static void main(String[] args) {
-      AStack s1 = new AStack();
-      Client1.useAStack(s1);
-      Client1.AuditorAStack(s1);
-      s1.print();
       LStack s2 = new LStack();
       Client2.useLStack(s2);
       Client2.AuditorLStack(s2);
