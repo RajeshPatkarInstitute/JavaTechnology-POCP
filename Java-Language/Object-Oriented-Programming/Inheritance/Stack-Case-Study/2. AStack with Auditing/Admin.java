7575c2da5aa18120fe98582d1907856d239e8cd2
Rajesh Patkar <patkar@rajeshpatkar.com>
2017-06-16 19:52:19 +0530

    Create Admin.java

---
+++ Java-Language/Object-Oriented-Programming/Inheritance/Stack-Case-Study/2. AStack with Auditing/Admin.java
@@ -0,0 +1,10 @@
+package com.rajeshpatkar;
+
+public class StackCaseStudy {
+    public static void main(String[] args) {
+      AStack s1 = new AStack();
+      Client1.useAStack(s1);
+      Client1.AuditorAStack(s1);
+      s1.print();
+    }
+}
