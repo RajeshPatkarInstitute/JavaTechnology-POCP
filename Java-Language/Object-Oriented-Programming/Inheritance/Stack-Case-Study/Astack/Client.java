94fc74c437148aa9125507b22b1612c59a3ac3c1
Rajesh Patkar <patkar@rajeshpatkar.com>
2017-06-16 19:38:24 +0530

    Create Client.java
    Client consumes the service provided by AStack

---
+++ Java-Language/Object-Oriented-Programming/Inheritance/Stack-Case-Study/Astack/Client.java
@@ -0,0 +1,20 @@
+package com.rajeshpatkar;
+
+public class Client1 {
+
+    public static void useAStack(AStack s){
+        
+        s.push(10);
+        System.out.println("Client1 pushed 10");
+        
+        s.push(20);
+        System.out.println("Client1 pushed 20");
+        
+        s.push(30);
+        System.out.println("Client1 pushed 30");
+        
+        System.out.println("Client1 popped "+s.pop());
+        
+    }
+    
+}
